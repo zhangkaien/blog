a1904061c836322344c3106bd646b103568bf6a3
kevin <381869882@qq.com>
2017-08-20 11:26:00 +0800

    1
    1

--- js/index.js
+++ js/index.js
@@ -1,11 +1,63 @@
-// document.onload = function(){
-
-// }
-// alert(window.innerWidth);
-var div2Width;
-
-$(document).ready(
-
-	
-
+$(document).ready(function(){
+	openProg();
+	setTimeout(function(){
+		closeProg();
+	},1550);
+	loadList(addTitlelistEvent);
 });
+
+// 进度条
+function openProg() {
+	var progElem = $(".out_prog");
+	progElem.css("display","block");
+	$(".in_prog").animate({"width":"90%"},500);
+}
+function closeProg() {
+	var progElem = $(".out_prog");
+	progElem.css("display","none");
+	$(".in_prog").css("width","0px");
+}
+
+// 加载标题列表
+function loadList(fun){
+	var htmlobj=$.ajax({
+		url:"/title.txt",
+		async:false,
+		contentType: "application/x-www-form-urlencoded; charset=utf-8" 
+	});
+	var list = htmlobj.responseText.split('\n'),
+		h = '';
+	for(var i = list.length-2;i >= 0;i--){
+		var l = list[i].split(' ');
+		var time = new Date(parseInt(l[1].split('.')[0]));
+		var t = time.getMonth()+1 + '/' + time.getDate();  
+		h += "<li><a href='"+ l[1] +"'>"+ "<i style='font-size:24px'>"+t+"</i>  "+l[0] +"</a></li>"
+	}
+ 	$(".title_list ul").html(h);
+ 	fun();
+}
+
+// 为列表添加事件
+function addTitlelistEvent(){
+	$(".title_list ul li a").click(function(event){
+		openProg();
+		setTimeout(function(){
+			closeProg();
+		},2000);
+		url = '/text/' +　$(this).attr('href');
+		var htmlobj=$.ajax({
+			url:url,
+			async:false,
+			contentType: "application/x-www-form-urlencoded; charset=utf-8",
+		});
+		$(".title_list .passage").html(htmlobj.responseText);
+		$(".title_list ul").css('display','none');
+		$(".title_list pre").css('display','block');
+		closeProg();
+		return false;
+	});
+	$(".list_btn").click(function(){
+		$(".title_list ul").css('display','block');
+		$(".title_list pre").css('display','none');
+	});
+}
